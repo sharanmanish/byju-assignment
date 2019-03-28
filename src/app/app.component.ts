db194c3db841fdf31b99e384bd95572359c3def5
sharanmanish <manishsharan@live.com>
2019-03-28 10:31:53 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,10 @@ export class AppComponent implements OnInit {
 
   subject;
 
+  chsubjects: any = ['Reset'];
+
+  chsubject;
+
   ngOnInit() {
     this.getData();
   }
@@ -105,45 +109,224 @@ export class AppComponent implements OnInit {
 
   }
 
+  fillChSubjects(upflag?) {
+    if (upflag) {
+      for (let key of this._datas) {
+        if (key["Child Subject"] !== "") {
+          if (this.chsubjects.includes(key["Child Subject"]) === false) {
+            this.chsubjects.push(key["Child Subject"]);
+          }
+        }
+      }
+    } else {
+      for (let key of this.datas) {
+        if (key["Child Subject"] !== "") {
+          if (this.chsubjects.includes(key["Child Subject"]) === false) {
+            this.chsubjects.push(key["Child Subject"]);
+          }
+        }
+      }
+    }
+  }
+
   fillAll(upflag?) {
     this.providers = this.providers.slice(0, 1);
     this.universities = this.universities.slice(0, 1);
     this.subjects = this.subjects.slice(0, 1);
+    this.chsubjects = this.chsubjects.slice(0, 1);
     this.fillProviders(upflag);
     this.fillUniversities(upflag);
     this.fillSubjects(upflag);
+    this.fillChSubjects(upflag);
   }
 
   update() {
-    if (this.provider === undefined && this.university === undefined && this.subject !== undefined) {
+    if (
+      this.chsubject === undefined &&
+      this.provider === undefined &&
+      this.university === undefined &&
+      this.subject !== undefined
+    ) {
       this.datas = [];
       this.datas = this._datas.filter(x => x["Parent Subject"] === this.subject);
       this.fillAll();
-    } else if (this.provider === undefined && this.university !== undefined && this.subject === undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider === undefined &&
+      this.university !== undefined &&
+      this.subject === undefined
+    ) {
       this.datas = [];
-      this.datas = this._datas.filter(x => x.Universities["Institutions"] === this.university);
+      this.datas = this._datas.filter(
+        x => x.Universities["Institutions"] === this.university
+      );
       this.fillAll();
-    } else if (this.provider === undefined && this.university !== undefined && this.subject !== undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider === undefined &&
+      this.university !== undefined &&
+      this.subject !== undefined
+    ) {
       this.datas = [];
-      this.datas = this._datas.filter(x => x.Universities["Institutions"] === this.university && x["Parent Subject"] === this.subject);
+      this.datas = this._datas.filter(
+        x =>
+          x.Universities["Institutions"] === this.university &&
+          x["Parent Subject"] === this.subject
+      );
       this.fillAll();
-    } else if (this.provider !== undefined && this.university === undefined && this.subject === undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider !== undefined &&
+      this.university === undefined &&
+      this.subject === undefined
+    ) {
       this.datas = [];
       this.datas = this._datas.filter(x => x["Provider"] === this.provider);
       this.fillAll();
-    } else if (this.provider !== undefined && this.university === undefined && this.subject !== undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider !== undefined &&
+      this.university === undefined &&
+      this.subject !== undefined
+    ) {
       this.datas = [];
-      this.datas = this._datas.filter(x => x["Provider"] === this.provider && x["Parent Subject"] === this.subject);
+      this.datas = this._datas.filter(
+        x => x["Provider"] === this.provider && x["Parent Subject"] === this.subject
+      );
       this.fillAll();
-    } else if (this.provider !== undefined && this.university !== undefined && this.subject === undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider !== undefined &&
+      this.university !== undefined &&
+      this.subject === undefined
+    ) {
       this.datas = [];
-      this.datas = this._datas.filter(x => x["Provider"] === this.provider && x.Universities["Institutions"] === this.university);
+      this.datas = this._datas.filter(
+        x =>
+          x["Provider"] === this.provider &&
+          x.Universities["Institutions"] === this.university
+      );
       this.fillAll();
-    } else if (this.provider !== undefined && this.university !== undefined && this.subject !== undefined) {
+    } else if (
+      this.chsubject === undefined &&
+      this.provider !== undefined &&
+      this.university !== undefined &&
+      this.subject !== undefined
+    ) {
       this.datas = [];
-      this.datas = this._datas.filter(x => x["Provider"] === this.provider && x.Universities["Institutions"] === this.university && x["Parent Subject"] === this.subject);
+      this.datas = this._datas.filter(
+        x =>
+          x["Provider"] === this.provider &&
+          x.Universities["Institutions"] === this.university &&
+          x["Parent Subject"] === this.subject
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider === undefined &&
+      this.university === undefined &&
+      this.subject === undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(x => x["Child Subject"] === this.chsubject);
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider === undefined &&
+      this.university === undefined &&
+      this.subject !== undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x["Parent Subject"] === this.subject
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider === undefined &&
+      this.university !== undefined &&
+      this.subject === undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x.Universities["Institutions"] === this.university
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider === undefined &&
+      this.university !== undefined &&
+      this.subject !== undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x.Universities["Institutions"] === this.university &&
+          x["Parent Subject"] === this.subject
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider !== undefined &&
+      this.university === undefined &&
+      this.subject === undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject && x["Provider"] === this.provider
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider !== undefined &&
+      this.university === undefined &&
+      this.subject !== undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x["Provider"] === this.provider &&
+          x["Parent Subject"] === this.subject
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider !== undefined &&
+      this.university !== undefined &&
+      this.subject === undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x["Provider"] === this.provider &&
+          x.Universities["Institutions"] === this.university
+      );
+      this.fillAll();
+    } else if (
+      this.chsubject !== undefined &&
+      this.provider !== undefined &&
+      this.university !== undefined &&
+      this.subject !== undefined
+    ) {
+      this.datas = [];
+      this.datas = this._datas.filter(
+        x =>
+          x["Child Subject"] === this.chsubject &&
+          x["Provider"] === this.provider &&
+          x.Universities["Institutions"] === this.university &&
+          x["Parent Subject"] === this.subject
+      );
       this.fillAll();
     } else {
+      this.datas = [];
       this.datas = this._datas;
       this.fillAll(true);
     }
